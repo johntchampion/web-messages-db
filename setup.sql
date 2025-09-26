144490fb4885a51fcbd02f0f401eac07e9e1bb99
John Champion <johnt.champion@gmail.com>
2025-09-26 14:30:26 -0400

    add a user table with triggers

--- setup.sql
+++ setup.sql
@@ -2,6 +2,25 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TYPE content_type AS ENUM ('text', 'image');
 
+CREATE TABLE users (
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    socket_id VARCHAR(32),
+    display_name VARCHAR(64) NOT NULL,
+    username VARCHAR(64) NOT NULL,
+    email VARCHAR(128) NOT NULL,
+    profile_pic_url VARCHAR(256),
+    hashed_password VARCHAR(128) NOT NULL,
+    activated BOOLEAN NOT NULL DEFAULT false,
+    activate_token VARCHAR(128),
+    activate_token_timestamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    reset_password_token VARCHAR(128),
+    reset_password_token_timestamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    user_id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+    UNIQUE(email),
+    UNIQUE(username)
+);
+
 CREATE TABLE conversations (
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
@@ -42,6 +61,27 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION update_auth_timestamps()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.activate_token IS DISTINCT FROM OLD.activate_token THEN
+        NEW.activate_token_timestamp = NOW();
+    END IF;
+
+    IF NEW.reset_password_token IS DISTINCT FROM OLD.reset_password_token THEN
+        NEW.reset_password_token_timestamp = NOW();
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_auth_timestamps BEFORE INSERT OR UPDATE ON users
+FOR EACH ROW EXECUTE PROCEDURE update_auth_timestamps();
+
+CREATE TRIGGER update_timestamp BEFORE UPDATE ON users
+FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
 
 CREATE TRIGGER update_timestamp_messages BEFORE UPDATE ON messages
 FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
